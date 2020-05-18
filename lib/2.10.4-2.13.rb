c7310106bcec48febc47ac1fd554ed934ea59748
takedatsutomu <mdhd.to@gmail.com>
2020-05-18 11:49:43 +0900

    2.10.6

--- lib/2.10.4-2.13.rb
+++ lib/2.10.4-2.13.rb
@@ -99,3 +99,52 @@ message
 
 # 2.11.1　デフォルト値付きの引数
 
+def greeting(country)
+    if country == 'japan'
+        'こんにちは'
+    else
+        'hello'
+    end
+end
+
+
+greeting
+greeting('us')
+greeting('us','japan')
+
+
+
+
+
+
+
+def greeting(country = 'japan')
+    if country = 'japan'
+        'こんにちは'
+    else
+        'hello'
+    end
+end
+
+
+greeting
+
+greeting('us')
+
+# デフォルト値付きの引数の混在
+def default_args(a,b,c = 0, d=0)
+    "a=#{a},b=#{b},c=#{c},d=#{d}"
+end
+default_args(1,2)
+default_args(1,2,3)
+default_args(1,2,3,4)
+
+def foo(time = Time.now,message = bar)
+    puts "time:#{time}message:#{message}"
+end
+
+def bar
+    'BAR'
+end
+
+foo
