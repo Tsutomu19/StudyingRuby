7d28098a485559359fe8af4d9aeabb7e4ac459f0
takedatsutomu <mdhd.to@gmail.com>
2020-05-23 14:04:14 +0900

    4.4.2

--- lib/4.1.rb
+++ lib/4.1.rb
@@ -24,7 +24,7 @@ a
 a = [1,2,3,1,2,3,]
 a.delete_if do |n|
     n.odd?
-end
+endCD
 
 
 # eachと異なり、ブロックの戻り値をチェックします。
@@ -56,3 +56,26 @@ numbers.each{|n| sum += n}
 sum
 
 # 省略してかく
+
+
+
+4.4
+# ブロックを使う配列のメソッド
+
+# 以下二つは同じ意味
+numbers = [1,2,3,4,5]
+new_numbers = []
+numbers.each {|n| new_numbers << n*10}
+new_numbers
+
+numbers = [1,2,3,4,5]
+new_numbers = numbers.map{|n| n}*10}
+new_numbers
+
+
+4.4.2
+numbers = [1,2,3,4,5,6]
+even_numbers = numbers.select {|n|n.even?}
+even_numbers
+
+selectメソッドは各要素に対してブロックを評価し、戻り値が真の要素を集めた配列を返すメソッドです。
