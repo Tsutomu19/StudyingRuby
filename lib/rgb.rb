fc61854d2278d13669407aed2e9ab8e4a8c053d3
takedatsutomu <mdhd.to@gmail.com>
2020-05-26 11:08:20 +0900

    4.7.3

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,5 +1,27 @@
 def to_hex(r,g,b)
-    '#000000'
+    [r,g,b],inject('#') do |hex,n|
+    hex + n.to_s(16).rjust(2,'0')
+    end
 end
+
+def to_ints(hex)
+    [0,0,0]
+end
+
+
+
+# injectメソッドは畳み込み演算を行うメソッドです。
+# 注入する
+# numbers = [1,2,3,4]
+# sum = 0
+# numbers.each { |n| sum += n}
+# sum
+
+
+
+# numbers = [1,2,3,4]
+# numbers.inject(0) { |result,n| result += n}
+# sum
+
 
 
--- test/rgb_test.rb
+++ test/rgb_test.rb
@@ -41,3 +41,31 @@ end
 
 
 4.6.3
+
+require 'minitest/auutorun'
+require './lib/rgb'
+
+class RgbTest < Minitest::Test
+    def test_to_hex
+        assert_equal'#000000', to_hex(0,0,0)
+        assert_equal'#ffffff', to_hex(255,255,255)
+        assert_equal'#043c78',to_hex(4,60,120)
+    end
+
+    def test_to_ints
+        assert_equal [0,0,0],to_ints('#000000')
+        assert_equal[255,255,255].to_ints('#ffffff')
+    end
+end
+
+
+def to_ints(hex)
+    r = hex[1..2]
+    g = hex[3..4]
+    b = hex[5..6]
+    ints = []
+    [r,g,b].each do |s|
+        ints << s.hex
+    end
+    ints
+end
