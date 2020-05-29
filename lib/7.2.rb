25545bff64a5a219134bb0aed612ec4760854a27
takedatsutomu <mdhd.to@gmail.com>
2020-05-29 11:28:26 +0900

    -4.9.4

--- lib/7.2.rb
+++ lib/7.2.rb
@@ -60,3 +60,102 @@ end
 # クラスはこのように、内部にデータを保持し、
 # さらに自分が保持しているデータを利用する独自のメソッドを持つことができます。
 # データとそのデータに関するメソッドが常にセットになるのでクラスを使わない場合に比べてテータとメソッドの整理がしやすくなります。
+
+
+
+7.3
+class User
+
+end
+
+class OrderItem
+
+end
+
+User.new
+この時呼ばれるのがinitializeメソッドです。
+インスタンスを初期化するために実行したい処理があれば、
+このinitializeメソッドでその処理を実行します。
+
+class User
+    def initialize
+        puts 'Initialized'
+    end
+end
+
+User.new
+
+user = User.new
+user.initialize
+# =>NoMethodError
+
+class User
+    def initialize(name,age)
+        puts "name:#{name},age:#{age}"
+    end
+end
+User.new
+# Error
+
+User.new('Alice',20)
+
+
+
+class User
+    def hello
+        "Hello"
+    end
+end
+
+user User.new
+# => "Hello"
+
+
+
+
+class User
+    def initialize(name)
+        @name = name
+    end
+    
+    def hello 
+        "Hello,I am  #{@name}"
+    end
+end
+user = User.new('Alice')
+user.hello
+
+
+class User
+    def hello
+        # shuffled_name = @name.chars.shuffle.join
+        "Hello,I am #{shuffled_name}."
+
+    end
+end
+
+user = User.new('Alice')
+user.hello
+
+
+
+
+クラスに関連は深いものの、一つ一つのインスタンスに含まれるデータは
+使わないメソッドを定義したい場合もある。
+そのような場合はクラスメソッドを定義した方が使い勝手がよくなります。
+クラスソッドの定義の仕方は以下
+
+class  クラス名
+    def self.クラスメソッド
+        # 処理
+    end
+end
+
+もう一つの書き方
+class クラス名
+    class << self
+        def クラスメソッド
+        end
+    end
+end
+
