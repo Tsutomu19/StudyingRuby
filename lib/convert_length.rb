2ee1a2a29648dced2019304a6a44af1b4782965d
takedatsutomu <mdhd.to@gmail.com>
2020-06-01 11:46:03 +0900

    5.5.3

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -1,4 +1,28 @@
-def convert_length(length,unit_from,unit_to)
-    39.37
+# def convert_length(length,from: :m,to: :m)
+#     units = {m:1.0,ft: 3.28,in:39.37,}
+#     (length/units[from]*units[to]).round(2)
+# end
+
+
+
+
+
+# メソッドのなかで作成しているハッシュについて見直してみましょう。
+# このハッシュは時にキーや値が追加されたり変更されたりしないので、メソッドを実行するたびに作り直す必要はありません。
+# こいういオブジェクトは、メソッドのそとで定数として、保持しておく方が実行効率がよくなります。
+# というわけ次のようにハッシュをメソッドの外に移動させ、定数化します。
+
+
+
+
+# 5.5.3 convert_lengthメソッドを改善する
+
+UNITS = {m: 1.0,ft: 3.28, in: 39.37}
+def convert_length(length,from: :m, to: :m)
+    (length/UNITS[from]*UNITS[to]).round(2)
 end
+
+
+
+
 
--- test/convert_length_test.rb
+++ test/convert_length_test.rb
@@ -1,12 +1,14 @@
 # 5,5,1
 # 5,5,2　いろんなん単位を変換できるようにする
+# 5,5,3　メソッドを改善
+
 require 'minitest/autorun'
 require './lib/convert_length'
 class ConvertLengthTest < Minitest::Test
     def test_convert_length
-        assert_equal 39.37,convert_length(1,'m','in')
-        assert_equal 0.38,convert_length(15,'in','m')
-        assert_equal 10670.73,convert_length(35000,'ft','m')
+        assert_equal 39.37,convert_length(1,from: :m, to: :in)
+        assert_equal 0.38,convert_length(15,from: :in,to: :m)
+        assert_equal 10670.73,convert_length(35000,from: :ft,to: :m)
     end
 end
 
@@ -16,10 +18,10 @@ units = {'m'=>1.0,'ft'=>3.28,'in'=>39.37}
 
 
 
-def convert_length(length,unit_from,unit_to)
-    units = {'m'=>1.0,'ft'=>3.28,'in'=>39.37}
-    (length/units[unit_from] * units[unit_to]).round(2)
-end
+# def convert_length(length,unit_from,unit_to)
+#     units = {'m'=>1.0,'ft'=>3.28,'in'=>39.37}
+#     (length/units[unit_from] * units[unit_to]).round(2)
+# end
 
 
 
@@ -27,4 +29,16 @@ end
 
 # 返還前の単位の長さ÷返還前の単位の比率 * 変換ごの単位の比率
 # 1メートルをインチに直すのであれば、1÷1.0*39.37=39.37
+
+
+
+
+
+# メソッドを改善して見ましょう。
+# メソッドの引数には'm'や'ft'のような文字列をわたいsていますが、必ずしも
+# 文字列出なくても良い気がする。
+# また長さの単位はハッシュのキーにもなってます。こういう時に最適なのが。。。シンボルです！
+# 長さの単位は文字列ではなくシンボルを渡すようにしてみましょう。
+
+
 
