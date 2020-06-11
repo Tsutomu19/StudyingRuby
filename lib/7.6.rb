b85905a7ac2ff9642c3b4614a0f58d2c97e964fd
takedatsutomu <mdhd.to@gmail.com>
2020-06-11 16:29:14 +0900

    [need_to_study]7.6.5

--- lib/7.6.rb
+++ lib/7.6.rb
@@ -130,3 +130,40 @@ Objectクラス以外のクラスを継承する場合は次のような構文�
 class DVD < Product 
 end
 
+7.6.5 superでスーパクラスのメソッドを呼びだす
+
+ProductクラスとDVDクラスに属性を追加してみましょう。
+Productクラスは商品名と価格と言う属性を持つことにします。
+
+class Product 
+    attr_reader :name, :price
+
+    def initialize(name, price)
+        @name = name 
+        @price = price
+    end
+end
+product = Product.new('A great monie', 1000)
+product.name
+product.price
+
+
+
+
+class DVD < Product
+    attr_reader :running_time
+
+    def initialize(name,price,running_time)
+        @name = name 
+        @price = price 
+        @running_time = running_time
+    end
+end
+dvd = DVD.new('A great movie', 1000, 120)
+dvd.name
+dvd.price
+dvd.running_time
+
+
+
+
